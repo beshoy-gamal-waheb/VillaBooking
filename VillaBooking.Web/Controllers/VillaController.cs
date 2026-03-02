191012120167760b9aa47cf03d177cf7c51fca9c
Beshoy Gamal <beshoygamall12@gmail.com>
2026-03-02 16:15:23 +0200

    feat: add villa index page with controller action and responsive UI

---
+++ VillaBooking.Web/Controllers/VillaController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VillaBooking.DTO.Responses;
+using VillaBooking.DTO.Villa;
+using VillaBooking.Web.Services.IServices;
+
+namespace VillaBooking.Web.Controllers
+{
+    public class VillaController(IVillaService _villaService) : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            List<VillaDTO> villaDTOs = new();
+            try
+            {
+                var response = await _villaService.GetAllAsync<APIResponse<List<VillaDTO>>>("");
+                if(response != null && response.Success && response.Data != null)
+                {
+                    villaDTOs = response.Data;
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["Error"] = $"An error occurred while retrieving villa data: {ex.Message}";
+            }
+
+            return View(villaDTOs);
+        }
+    }
+}
